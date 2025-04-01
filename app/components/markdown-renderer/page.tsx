75079dde4a95ee1b1a25ea3ef464780be06e222a
Awesome <awesome_devnet@outlook.com>
2025-04-01 14:58:58 +0900

    fix(markdown): fix dummy

--- app/components/markdown-renderer/page.tsx
+++ app/components/markdown-renderer/page.tsx
@@ -541,7 +541,7 @@ a_{31} & a_{32} & a_{33} \\\\
 \\end{pmatrix}
 $$
 
-\* Inline cases..
+\\* Inline cases..
 
 $$A = \\begin{pmatrix} a_{11} & a_{12} & a_{13} \\\\ a_{21} & a_{22} & a_{23} \\\\ a_{31} & a_{32} & a_{33} \\\\\\end{pmatrix}$$
 
