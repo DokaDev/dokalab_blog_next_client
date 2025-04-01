5e9d3d4d922722a9b51b10d7853966d453cda75c
Awesome <awesome_devnet@outlook.com>
2025-04-01 14:56:20 +0900

    update

--- app/components/markdown-renderer/page.tsx
+++ app/components/markdown-renderer/page.tsx
@@ -537,9 +537,13 @@ $$
 A = \\begin{pmatrix} 
 a_{11} & a_{12} & a_{13} \\\\
 a_{21} & a_{22} & a_{23} \\\\
-a_{31} & a_{32} & a_{33}
+a_{31} & a_{32} & a_{33} \\\\
 \\end{pmatrix}
 $$
+
+\* Inline cases..
+
+$$A = \\begin{pmatrix} a_{11} & a_{12} & a_{13} \\\\ a_{21} & a_{22} & a_{23} \\\\ a_{31} & a_{32} & a_{33} \\\\\\end{pmatrix}$$
 
 Quantum operators:
 
