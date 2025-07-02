dfd58545fb388fe5b67442210f8c851dfa1cdf60
Awesome <awesome_devnet@outlook.com>
2025-07-02 11:18:01 +0900

    update

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import Link from 'next/link';
-import styles from './page.module.scss';
+import { useEffect } from "react";
+import Link from "next/link";
+import styles from "./page.module.scss";
 
 export default function AdminDashboardPage() {
   useEffect(() => {
@@ -12,16 +12,17 @@ export default function AdminDashboardPage() {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>관리자 대시보드</h2>
-      
+
       <div className={styles.actionCard}>
         <h3>콘텐츠 관리</h3>
         <div className={styles.actionLinks}>
           <Link href="/admin/editor" className={styles.actionLink}>
-            마크다운 에디터
+            markdown editor
           </Link>
           {/* 추가 링크들 */}
         </div>
       </div>
     </div>
   );
-} 
+}
+
